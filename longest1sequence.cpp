83bd0cf5a75a8974fe864cdb2b95a9f20c4987c8
Fasuplusplus <facundo_forte@hotmail.com>
2021-05-25 02:14:41 -0300

    commit on master

--- longest1sequence.cpp
+++ longest1sequence.cpp
@@ -1,36 +1,31 @@
 //encontrar la secuencia de 1s o 0s constantes más larga en un array de longitud indefinida con un -1 como marcador de final
 #include <iostream>
-int longestSequence(int bit, int input[])   //el valor de bit es el número del que va a buscar la secuencia. Input es la secuencia.
-{ 
+int longestSequence(int bit, int input[]) //el valor de bit es el número del que va a buscar la secuencia. Input es la secuencia.
+{
     int partialCount = 0;
     int count = 0;
     int i = 0;
-    while (input[i] != (-1))
-    {
-        if (input[i] == bit)
-        {
+    while (input[i] != (-1)) {
+        if (input[i] == bit) {
             partialCount++;
         }
-        else if (partialCount > count)
-        {
+        else if (partialCount > count) {
             count = partialCount;
             partialCount = 0;
         }
-        else
-        {
+        else {
             partialCount = 0;
         }
         i++;
     }
-    if (partialCount > count)
-    {
+    if (partialCount > count) {
         count = partialCount;
         partialCount = 0;
     }
     return count;
 }
-int main(){
+int main() {
     int input[]{1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, -1};
     short int i = 1;
-    std::cout << "La secuencia mas larga de " << i << "s es de "<< (longestSequence(i, input)) << " digitos" << "\n";
+    std::cout << "La secuencia mas larga de " << i << "s es de " << (longestSequence(i, input)) << " digitos\n";
 }
